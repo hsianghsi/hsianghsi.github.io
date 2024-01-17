3cda31293595d62ea4c105ada4a1733d8e0b1853
bossbossleu <bossbossleu@gmail.com>
2024-01-17 17:27:36 +0800

    upload_all

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,30 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addCollection('posts', function(collection) {
-      return collection.getFilteredByGlob('posts/*.md');
+        return collection.getFilteredByGlob('_posts/*.md');
     });
-  
+
+    eleventyConfig.addFilter("slugify", function(str) {
+        if (!str || typeof str !== 'string') {
+            return '';
+        }
+        return str.replace(/\s+/g, '-').toLowerCase();
+    });
+
     return {
-      markdownTemplateEngine: "liquid",
-      dir: {
-        input: ".",
-        output: "_site",
-      },
+        markdownTemplateEngine: "liquid",
+        dir: {
+            input: ".",
+            output: "_site",
+            includes: "_includes",
+            data: "_data",
+        },
+        passthroughFileCopy: true,
+        htmlTemplateEngine: "liquid",
+        templateFormats: ["html", "liquid", "md"],
+        permalink: "/_site{{ post.url }}",
     };
 };
-  
+
+
+
+
